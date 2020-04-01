eb654983013bdc4a259adf90d8cf48d4893983c2
kevin <keebvhev@gmail.com>
2020-04-02 00:05:05 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,34 @@
+//1. (A)
+
+//f("junyiacademy") == "ymedacaiynuj"
+
+function reverse(str) {
+  // console.log(str)
+  var arr = str.split("")
+  aaa = []
+  for (i = 0; i < arr.length; i++) {
+    // console.log(arr[i])
+    aaa[arr.length - i] = arr[i]
+  }
+  // console.log(aaa.toString())
+  return aaa.toString
+}
+
+reverse("junyiacademy")
+
+//1.(B)
+
+//2.
+
+//３.Anser
+//拿只有一個東西的代子，如果是鉛筆，則另一個只有一個東西的代子就是原子筆。
+//有兩個東西的一定是有鉛筆與原子筆
+
+
+// 4.Anser 
+// 三人出了 900 - 90 = 810元也就是店員拿到的
+// 三人手上有　90元
+
+//　拿店員手上的 810元　去加上 60元　說不通，因為６０元已經在他手上了。
+
+// 應該把店員手上的加上三個人手上的錢才是 900元
